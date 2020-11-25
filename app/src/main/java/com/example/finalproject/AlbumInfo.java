7658e7115012e9a754e92dc97e1e3d7623522412
Adam O'Brien <obri0056@algonquinlive.com>
2020-11-24 23:25:17 -0500

    Update

---
+++ app/src/main/java/com/example/finalproject/AlbumInfo.java
@@ -0,0 +1,22 @@
+package com.example.finalproject;
+
+
+public class AlbumInfo {
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
+
+    String albumName;
+
+    public AlbumInfo(String n){
+this.albumName = n;
+
+    }
+
+
+}
