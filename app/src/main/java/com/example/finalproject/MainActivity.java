48e48220de629ce0957af864aaa031c48d0da556
Adam O'Brien <obri0056@algonquinlive.com>
2020-12-07 22:18:24 -0500

    strings

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,91 +1,39 @@
 package com.example.finalproject;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.AlertDialog;
 import android.content.Intent;
-import android.content.res.Configuration;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.widget.Button;
 
-import com.google.android.material.navigation.NavigationView;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
-    ActionBarDrawerToggle toggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent audioPage = new Intent(this, AudioDB.class);
+        Intent covidPage = new Intent(this, Covid19Main.class);
+        Button audioButton = findViewById(R.id.audioActivityButton);
+        Button covidButton = findViewById(R.id.covidButton);
 
-        Toolbar toolBar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolBar);
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        toggle = new ActionBarDrawerToggle(this, drawer, toolBar, R.string.open, R.string.close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-        Log.e ("Main","Action -3");
-        NavigationView navigationView = findViewById(R.id.navigation);
-        navigationView.setNavigationItemSelectedListener(this);
-        navigationView.setItemIconTintList(null);
-    }
+        covidButton.setOnClickListener(clk->{
+            startActivity(covidPage);
+        });
+        audioButton.setOnClickListener(clk -> {
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            alertDialog.setTitle("Are you sure?");
+            alertDialog.setMessage("Would you like to continue to AudioDB?");
+            alertDialog.setPositiveButton("Yes", (click, arg) -> {
+                startActivity(audioPage);
+            });
+            alertDialog.setNegativeButton("No", (click, arg) -> {
+            });
+            alertDialog.create();
+            alertDialog.show();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.toolbar_menu, menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId())
-        {
+        });
 
-            case R.id.covid:
-                Intent goToLyricSong = new Intent(this, Covid19Main.class);
-                startActivity(goToLyricSong);
-
-        }
-
-        return true;
-    }
-
-    @Override
-    public boolean onNavigationItemSelected( MenuItem item) {
-
-        switch(item.getItemId())
-        {
-
-            case R.id.covid:
-                Intent goToLyricSong = new Intent(this, Covid19Main.class);
-                startActivity(goToLyricSong);
-                break;
-
-        }
-
-        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
-        drawerLayout.closeDrawer(GravityCompat.START);
-
-        return false;
-    }
-
-    @Override
-    public void onPostCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onPostCreate(savedInstanceState, persistentState);
-        toggle.syncState();
-    }
-
-    @Override
-    public void onConfigurationChanged(@NonNull Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        toggle.onConfigurationChanged(newConfig);
     }
 }
